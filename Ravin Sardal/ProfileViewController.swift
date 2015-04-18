52a7921a884eb70f3f9c5566714da203ee5dd688
Ravin Sardal <ravin.sardal@gmail.com>
2015-04-18 09:43:01 -0700

    Basic Setup

--- Ravin Sardal/ProfileViewController.swift
+++ Ravin Sardal/ProfileViewController.swift
@@ -1,26 +1,28 @@
 //
-//  ViewController.swift
+//  ProfileViewController.swift
 //  Ravin Sardal
 //
-//  Created by Ravin Sardal on 4/15/15.
+//  Created by Ravin Sardal on 4/18/15.
 //  Copyright (c) 2015 Ravin Sardal. All rights reserved.
 //
 
 import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-
+class ProfileViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func sayHello(sender: AnyObject) {
+        println("Hello")
+    }
+    
 }
-
