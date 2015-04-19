d852f739e85b90f1800e283f1733aaca9468265e
Ravin Sardal <ravin.sardal@gmail.com>
2015-04-19 11:25:54 -0700

    Part 3 commment

--- Ravin Sardal/ViewController.swift
+++ Ravin Sardal/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-        // What's up dude? Part 2
+        // What's up dude? Part 2 and part 3
     }
 
 
