5738c62faf7e5c4501c252e4a255777f82ebc840
Sidhant Uthra <sidhantuthra@dhcp-v14-00151.highsouth-resnet.unc.edu>
2015-04-19 15:12:04 -0400

    Removed testing comments

--- Ravin Sardal/ViewController.swift
+++ Ravin Sardal/ViewController.swift
@@ -19,7 +19,6 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-        // What's up dude? Part 2 and part 3
     }
 
 
