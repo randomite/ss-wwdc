d1b105f45f6c6fbb75ccfa4aa2d0fb6cac14f82c
Ravin Sardal <ravin.sardal@gmail.com>
2015-04-19 22:33:05 -0700

    Create new Interests VC and fix sizing issues for splash page

--- Ravin Sardal/InterestsViewController.swift
+++ Ravin Sardal/InterestsViewController.swift
@@ -1,17 +1,15 @@
 //
-//  AppleInfoViewContoller.swift
+//  InterestsViewController.swift
 //  Ravin Sardal
 //
 //  Created by Ravin Sardal on 4/19/15.
 //  Copyright (c) 2015 Ravin Sardal. All rights reserved.
 //
 
-
-
 import Foundation
 import UIKit
 
-class AppleInfoViewContoller: UIViewController {
+class InterestsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,12 +20,6 @@ class AppleInfoViewContoller: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    @IBAction func applePrintln(sender: AnyObject) {
-        println("works")
-    }
-    @IBAction func backToHome(sender: AnyObject) {
-        println("works")
-    }
-    
+  
+   
 }
