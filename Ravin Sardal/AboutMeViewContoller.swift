1915e1b6fa3b99a363dde026549595cf7f0fd1dc
Ravin Sardal <ravin.sardal@gmail.com>
2015-04-23 19:50:28 -0700

    Profile view updated, animation and typewriter effect added

--- Ravin Sardal/AboutMeViewContoller.swift
+++ Ravin Sardal/AboutMeViewContoller.swift
@@ -11,9 +11,20 @@ import UIKit
 
 class AboutMeViewContoller: UIViewController {
     
+    @IBOutlet var myDescriptionTextView: UITextView!
+    @IBOutlet var myNameLabel: UILabel!
+    let myNameArray = Array("Hello, my name is Ravin!")
+    var myCounter = 0
+    var timer:NSTimer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        fireTimer()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        self.myDescriptionTextView.alpha = 0;
     }
     
     override func didReceiveMemoryWarning() {
@@ -21,11 +32,25 @@ class AboutMeViewContoller: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func aboutMePrint(sender: AnyObject) {
-        println("works")
-    }
     
-    @IBAction func backToHome(sender: AnyObject) {
-        println("works")
+    func fireTimer(){
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.5, target: self, selector: "typeLetter", userInfo: nil, repeats: true)
     }
+    func typeLetter(){
+        if myCounter < myNameArray.count {
+            myNameLabel.text = myNameLabel.text! + String(myNameArray[myCounter])
+            let randomInterval = Double((arc4random_uniform(8)+1))/20
+            timer?.invalidate()
+            timer = NSTimer.scheduledTimerWithTimeInterval(randomInterval, target: self, selector: "typeLetter", userInfo: nil, repeats: false)
+        } else {
+            timer?.invalidate()
+            UIView.animateWithDuration(1.0, animations: {
+                self.myDescriptionTextView.alpha = 1.0
+            })
+        }
+        myCounter++
+    }
+
+    
+
 }
