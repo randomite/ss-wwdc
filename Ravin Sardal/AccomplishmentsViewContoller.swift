4dd6b55d9585a45e71c085730ce3828b6956805e
Sidhant Uthra <sidhantuthra@dhcp-v14-00151.highsouth-resnet.unc.edu>
2015-04-24 01:53:26 -0400

    Tweaked the info in the Accomplishments VC

--- Ravin Sardal/AccomplishmentsViewContoller.swift
+++ Ravin Sardal/AccomplishmentsViewContoller.swift
@@ -22,7 +22,7 @@ class AccomplishmentsViewContoller: UIViewController,UIPickerViewDelegate,UIPick
         // Do any additional setup after loading the view, typically from a nib.
         arr = ["FBLA", "Apple Apps", "Summer Internship", "PebbleApps","Apple WatchKit Tutorials", "Hackathons"]
         
-        self.myTextView.text = "Last year in Homestead Highschool I was part of a club called Future Business Leaders of America. I choose to take part in technology-related commpetitions. Last summer I placed First in California State for Database Design and Applications and went on to represent my school in Nationals."
+        self.myTextView.text = "While attending Homestead High School, I participated in technology competitions with the Future Business Leaders of America club. I placed first in the Database Design and Applications event at the California state competition and went on to the National competition."
         self.hideButtonsShowText()
         
     }
@@ -76,16 +76,16 @@ class AccomplishmentsViewContoller: UIViewController,UIPickerViewDelegate,UIPick
             self.matherobicAppButton.hidden = false;
         }else if(row == 2){
             self.hideButtonsShowText()
-            self.myTextView.text = "This coming summer I will be an iOS intern for Ford Motors."
+            self.myTextView.text = "This summer I will intern at Ford Motor Company in the iOS Development division and will be working on Apple Watch apps for vehicles. I am beyond excited for the opportunity to impact the lives of a plethora of drivers."
         }else if(row == 3){
             self.hideButtonsShowText()
-            self.myTextView.text = "I have developed various Pebble SmartWatch Applications for the Pebble SmartWatch. These applications range from hundreds to thousands of users each."
+            self.myTextView.text = "I have developed various Pebble SmartWatch Applications for the Pebble SmartWatch that have thousands of users each. One example is ________, which serves the purpose to __________."
         }else if(row == 4){
             self.hideButtonsShowText()
-            self.myTextView.text = "I make tutorials on youtube for people to learn Apple WatchKit development. I have over 25,000 views and a few hundred subscribers."
+            self.myTextView.text = "I maintain a YouTube channel in which I make tutorials for users to learn Apple WatchKit development and am proud that this channel has over 25,000 views and a few hundred subscribers. A friend of mine started watching my videos and is now working on his own Watch app. It feels incredible to know that I have inspired someone to innovate."
         }else if(row == 5){
             self.hideButtonsShowText()
-            self.myTextView.text = "I have won many hackathons, here is a list:"
+            self.myTextView.text = "Coding is my passion. One way I like to indulge in this obsession is participating in hackathons. I have won many hackathons such as __________."
         }
 
     }
